2b277f05bb6422f4bc8608ad9211fbd7edcae6fc
jeongjeahyoun <zeni9@naver.com>
2017-05-16 08:24:29 +0000

    first commit

---
+++ db/migrate/20170516081929_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      
+        t.string "title"
+        t.string "writer"
+        t.string "content"
+        t.integer "hit"
+
+
+      t.timestamps null: false
+    end
+  end
+end
